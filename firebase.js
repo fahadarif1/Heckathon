f5cd82d3366f447b733fd8d98038e9651411c539
Fahad-Arif <134074010+fahadarif1@users.noreply.github.com>
2024-09-20 05:03:03 -0700

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBnum1eky2YL-aG45QOGt30GzMDCpqAIfA",
+    authDomain: "login-page-70307.firebaseapp.com",
+    projectId: "login-page-70307",
+    storageBucket: "login-page-70307.appspot.com",
+    messagingSenderId: "153396521720",
+    appId: "1:153396521720:web:22d1c70172fff810b6a90d"
+};
+
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
